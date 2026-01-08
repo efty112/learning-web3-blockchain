825c1ccadbf5cfc8e28aa345e6a3ef34a78a3a59
Mohtasim <wronglive67@gmail.com>
2026-01-09 04:15:02 +0800

    add navbar and gradient color

--- PROJECTS/1-web-based-wallet/app/page.tsx
+++ PROJECTS/1-web-based-wallet/app/page.tsx
@@ -1,65 +1,50 @@
 import Image from "next/image";
+import walletLogo from "../public/logo.png"
+import bg1 from "@/public/image1.png"
+import bg2 from "@/public/image2.png";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="h-screen bg-black max-w-full mx-auto px-16 py-5">
+
+      <nav className="text-black bg-white/35 flex items-center justify-between gap-x-3 px-5 py-2 rounded-4xl">
+
+        <div className="flex items-center justify-center gap-2">
+          <Image alt="Shadhin Wallet Logo" src={walletLogo} width={40} quality={75} preload={true}></Image>
+          <p className="font-bold text-2xl">SHADHIN WALLET</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="flex gap-x-5">
+            <div>
+            Home
+          </div>
+          
+          <div>
+            About Us
+          </div>
+          
+          <div>
+            Contact Us
+          </div>
+          
+          <div>
+            Create Wallet
+          </div>
+          
         </div>
-      </main>
+      </nav>
+      
+      <div className="my-12">
+        <div>
+          <p>We Are</p>
+          <h1>SHADHIN WALLET</h1>
+        </div>
+        <div className="flex justify-end">
+          <div className="w-[600px] h-[600px] bg-[linear-gradient(260deg,#8750f7,rgba(115,67,210,0)_100%)] rounded-full blur-[150px] absolute"></div>
+          <Image alt="background-image" src={bg1} quality={100} width={400}></Image>
+        </div>
+      </div>
+      
     </div>
   );
 }
